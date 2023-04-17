0f8ad1434529aadd187914f7f04fa2878656e7a0
joparedesm <joparedesm@unal.edu.co>
2023-04-16 21:27:14 -0500

    Employees endpoints

--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -4,7 +4,13 @@ import config from '../config.js';
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello World! \n Dummy key: ' + config.testKey);
+    res.send('Get employees! \n Dummy key: ' + config.testKey);
 });
+
+router.post('/', (req, res) => res.send('Create employee!'));
+
+router.put('/', (req, res) => res.send('Update employee!'));
+
+router.delete('/', (req, res) => res.send('Delete employee!'));
 
 export default router;
