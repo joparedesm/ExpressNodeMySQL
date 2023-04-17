7fd923b333969fecbe57ae4e2cb7d6550abdf7b3
joparedesm <joparedesm@unal.edu.co>
2023-04-17 00:33:59 -0500

    Add: POST/Create

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -1,10 +1,14 @@
 import config from '../config.js';
+import { pool } from '../db.js';
 
 export const getEmployees = (req, res) => {
     res.send('Get employees! \n Dummy key: ' + config.testKey);
 }
 
-export const createEmployee = (req, res) => res.send('Create employee!')
+export const createEmployee = async (req, res) => {
+    await pool.query('INSERT INTO employees (name, email, phone, salary) VALUES (?, ?, ?, ?)', [req.body.name, req.body.email, req.body.phone, req.body.salary]);
+    res.send('Employee created!');
+}
 
 export const updateEmployee = (req, res) => res.send('Update employee!')
 
